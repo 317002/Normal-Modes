0d1f2d7013ad57a36311d76964c80e03ffe9e3a5
Nathan <ndr1005@wildcats.unh.edu>
2019-10-31 17:18:10 -0400

    update

--- Main.py
+++ Main.py
@@ -7,3 +7,67 @@ Created on Thu Oct 31 13:36:31 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
+import objects as my
+
+
+
+#contants
+N = 7#should be odd
+bounds = [-10,10]
+test_spring = my.spring(1,.5,[1,0,0],[0,0,0])
+test_mass = my.particle(1,[0,0,0])
+
+
+
+
+###########################
+####Defining the figure####
+###########################
+
+fig = plt.figure(figsize = (12,8))
+ax = fig.add_subplot(111)
+ax.scatter(*test_mass.posistion,linewidth = 1)
+
+'''setting the bounds of the graph'''
+ax.set_xlim(bounds)
+ax.set_ylim(bounds)
+
+
+################################
+####Generating the particles####
+################################
+
+
+x = np.arange(-N,N + 1)
+y = x.copy()
+
+
+x = np.zeros((x.shape[0] + 1,1),dtype=x.dtype) + x
+y = x.transpose()
+z = np.zeros(x.shape)*0
+
+particles = []
+for line1,line2,line3 in zip(x,y,z):
+    for q1,q2,q3 in zip(line1,line2,line3):
+        part = my.particle(1,[q1,q2,q3])
+        particles.append(part)
+        ax.scatter(*part.posistion[0:2])
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- objects.py
+++ objects.py
@@ -10,23 +10,30 @@ Created on Thu Oct 31 12:08:45 2019
 import numpy as np
 
 
+def gen_spring_line(particle,spring):
+    vecs = []
+    for p,o in zip(particle.posistion,spring.orientation):
+        vecs.append(np.linspace(p,p + o*spring.l,2))
+    return(vecs)
 
+
+
+class particle:
+    def __init__(self,m,posistion,springs):
+        self.m = m
+        self.posistion = posistion#the posistion of the particle
 
 
 class spring:
-    def __init__(self,k,d,orientation):
+    def __init__(self,k,l,orientation,posistion):
         self.k = k#spring constant
-        self.d = d#lenght of the spring
-        self.orientation = orientation#[x,y,z] Ex: [1,1,-1]#unit vectors
+        self.l = l#lenght of the spring
+        self.orientation = orientation#[x,y,z] Ex: [1,0,0]#unit vectors
+        self.posistion = posistion
     
     def get_potential(self,x):
         return 1/2*(self.k*x**2)
         
-
-class partical:
-    def __init__(self,m,springs):
-        self.m = m
-        self.springs = springs#a list of spring objects acting on the particle
 
 
 
