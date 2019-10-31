813da1988b56777b39ecf2d6ba4c8860cd55225f
Nathan <ndr1005@wildcats.unh.edu>
2019-10-31 12:12:59 -0400

    initial commit

---
+++ Main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 12:08:45 2019
+
+@author: Nathan Richard
+"""
+
+
+'''Imports'''
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.linalg as alg
