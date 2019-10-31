504ad06d2f9c7fdd81a4315d09a7188bde12d2db
Nathan <ndr1005@wildcats.unh.edu>
2019-10-31 13:38:00 -0400

    Change to structure

--- Main.py
+++ Main.py
@@ -1,32 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct 31 12:08:45 2019
+Created on Thu Oct 31 13:36:31 2019
 
 @author: Nathan Richard
 """
 
-
-'''Imports'''
 import numpy as np
 import matplotlib.pyplot as plt
-import numpy.linalg as alg
-
-
-
-
-class spring:
-    def __init__(self,k,d,orientation):
-        self.k = k#spring constant
-        self.d = d#lenght of the spring
-        self.orientation = orientation#[x,y,z] Ex: [1,1,-1]#unit vectors
-    
-    def get_potential(self,x):
-        return 1/2*(self.k*x**2)
-        
-
-class partical:
-    def __init__(self,m,springs):
-        self.m = m
-        self.springs = springs#a list of spring objects
-        
-        
---
+++ objects.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 12:08:45 2019
+
+@author: Nathan Richard
+"""
+
+
+'''Imports'''
+import numpy as np
+
+
+
+
+
+class spring:
+    def __init__(self,k,d,orientation):
+        self.k = k#spring constant
+        self.d = d#lenght of the spring
+        self.orientation = orientation#[x,y,z] Ex: [1,1,-1]#unit vectors
+    
+    def get_potential(self,x):
+        return 1/2*(self.k*x**2)
+        
+
+class partical:
+    def __init__(self,m,springs):
+        self.m = m
+        self.springs = springs#a list of spring objects acting on the particle
+
+
+
+        
+        
